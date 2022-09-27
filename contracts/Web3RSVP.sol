8f31d21415ca2b371c84a8d3fe4dab77b921c222
Otaiki1 <otaikisadiq2804@gmail.com>
2022-09-27 17:46:41 +0100

    refac: create setter function for creating event

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -17,5 +17,38 @@ contract Web3RSVP{
     }
     // a mapping that maps a unique event id to the event
     mapping(bytes32 => CreateEvent) public idToEvent;
-    
+
+    //a function that creates new events
+    function createNewEvent(
+        uint256 eventTimestamp,
+        uint256 deposit,
+        uint256 maxCapacity,
+        string calldata eventDataCID
+    ) external {
+        //generate an eventID based on other things passed in to generate a hash
+        bytes32 eventId = keccak256(abi.encodePacked(
+                                        msg.sender,
+                                        address(this),
+                                        eventTimestamp,
+                                        deposit,
+                                        maxCapacity
+                                    )
+                                );
+
+        address[] memory confirmedRSVPs;
+        address[] memory claimedRSVPs;
+
+        //this creates a new CreateEvent struct and adds it to the idToEvent mapping
+        idToEvent[eventId] = CreateEvent(
+            eventId,
+            eventDataCID,
+            msg.sender,
+            eventTimestamp,
+            deposit,
+            maxCapacity,
+            confirmedRSVPs,
+            claimedRSVPs,
+            false
+        );
+    }
 }
